a25d626349fa8f89dc770c7e93f2d8528c4ad4bc
yuliusK <yuliuskurniawan26@gmail.com>
2026-06-02 23:06:15 +0700

    add menu: order tmmin, order ddmi update: status master suffix, parameter capacity, process, result, summary capacity

--- RFIDP2P3_Web/Controllers/DailyCalculationController.cs
+++ RFIDP2P3_Web/Controllers/DailyCalculationController.cs
@@ -11,5 +11,10 @@ namespace RFIDP2P3_Web.Controllers
             return View();
         }
         
+        [SessionAuthorize("read_OrderDdmi")]
+        public IActionResult OrderDdmi()
+        {
+            return View();
+        }
     }
 }
